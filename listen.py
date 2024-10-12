24e1698b6644600fe9e267a15c79031b2c14c6d8
Shahan Neda <shahan.neda@gmail.com>
2024-10-12 11:59:36 -0400

    added venv

---
+++ listen.py
@@ -0,0 +1,12 @@
+import pyaudio
+import wave
+
+def record_audio(filename, duration=5, sample_rate=44100, channels=1, chunk=1024unk.append(data)
+# Stop and close the streampwidth(audio.get_tframemple_rate)
+        wf.writeframes(b''.join(frames))
+
+    print(f"Audio saved to {filename}")
+
+if __name__ == "__main__":
+    record_audio("recorded_audio.wav")
+
